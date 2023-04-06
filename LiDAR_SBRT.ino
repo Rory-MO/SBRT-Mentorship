010e80a78e5d516f7635e23fd86e287d98a1aeaf
Esmaynard <123605089+Esmaynard@users.noreply.github.com>
2023-04-05 20:26:46 -0400

    Add files via upload

--- LiDAR_SBRT.ino
+++ LiDAR_SBRT.ino
@@ -1,6 +1,8 @@
 #include <RPLidar.h>
 #define RPLIDAR_MOTOR 12 // The PWM pin for control the speed of RPLIDAR's motor (MOTOCTRL).
 #define COLLISION_DISTANCE 200 // Min distnace before robot is forced to stop MUST BE GREATER THAN 70
+#define GO 10 // Orange LED indicates clear collision zone
+#define STOP 9 // Red LED indicates object in collision zone
 RPLidar lidar;
                       
 void setup() {
@@ -8,6 +10,8 @@ void setup() {
   Serial1.begin(115200);  // For RPLidar
   lidar.begin(Serial1);
   pinMode(RPLIDAR_MOTOR, OUTPUT);  // set pin modes
+  pinMode(GO, OUTPUT); 
+  pinMode(STOP, OUTPUT);
 }
 
 
@@ -18,12 +22,19 @@ void loop() {
     //perform data processing here... 
     float distance = lidar.getCurrentPoint().distance - 30; //Get distance in mm, 30mm offset
     float angle = lidar.getCurrentPoint().angle;  // 0-360 deg
-    //If the obstacle is within the collsion distance and the angle infront of the robot, stop
-     if((angle > 330 || angle < 30) && distance > 0 && distance < COLLISION_DISTANCE)  { 
+    //If the obstacle is within the collsion distance and infront of the robot, stop
+     if((angle > 350 || angle < 10) && distance > 0 && distance < COLLISION_DISTANCE)  { 
       printData(angle, distance);
+      digitalWrite(STOP, HIGH); // Turn on red LED
+      digitalWrite(GO, LOW); // Turn off orange LED
       //stop();
       //return;
      }
+     else if((angle > 350 || angle < 10) && distance > COLLISION_DISTANCE) {
+       digitalWrite(STOP, LOW); // Turn off red LED
+       digitalWrite(GO, HIGH); //Turn on orange LED
+     }
+    
   }
   else {
     analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor if unresponsive
@@ -46,3 +57,4 @@ void printData(float angle, float distance)
   Serial.print("    angle: ");
   Serial.println(angle);
 }
+
